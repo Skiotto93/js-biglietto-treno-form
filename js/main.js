430e3bdd713ba6431547be3c28475eb0acfba91e
skiotto93 <fabio.forenza@hotmail.com>
2022-10-24 17:58:45 +0200

    Aggiunta prima sezione

--- js/main.js
+++ js/main.js
@@ -1,23 +1,20 @@
 "use strict";
 // Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
-const distance = document.getElementById("distance").value;
-const age =  document.getElementById("age").value;
-const name = document.getElementById("name").value;
+
 const ticket = document.getElementById("ticket");
-let ticketPrice = 0;
 
 // Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
 // il prezzo del biglietto è definito in base ai km (0.21 € al km)
 // va applicato uno sconto del 20% per i minorenni
 // va applicato uno sconto del 40% per gli over 65.
 
-const generate = document.getElementById("generate");
-generate.addEventListener("click", 
+const btngenerate = document.getElementById("generate");
+btngenerate.addEventListener('click', 
     function()  {
         // Il programma leggerà dal form il numero di chilometri che vuole percorrere, il nome e la fascia d'età.
-        const name = name.value;
-        const distance = Number(distance.value);
-        const age = age.value; // minorenne, maggiorenne, over
+        const name = document.getElementById('name').value;
+        const distance = Number(document.getElementById('distance').value);
+        const age =  document.getElementById('age').value; // minorenne, maggiorenne, over
         // Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
         // il prezzo del biglietto è definito in base ai km (0.21 € al km)
         // va applicato uno sconto del 20% per i minorenni
@@ -32,6 +29,6 @@ generate.addEventListener("click",
             ticketPrice -= ticketPrice * 0.4;
             ticketType = "Biglietto scontato del 40%";
         }
+        console.log(ticketPrice);
     }
     );
-console.log(`${ticketPrice.toFixed(2)}€`);
